5e110c7aade6154677f4c70f8c1e7e7865557aa3
bluuweb <contacto@bluuweb.cl>
2022-08-25 21:29:39 -0400

    😍

---
+++ src/components/UserCardSkeletor.vue
@@ -0,0 +1,8 @@
+<template>
+    <article class="border shadow rounded-md p-4 max-w-sm w-full mx-auto mt-5">
+        <div class="animate-pulse flex flex-col items-center">
+            <div class="rounded-full bg-slate-200 h-32 w-32"></div>
+            <div class="h-4 w-40 mt-4 bg-slate-200 rounded-md"></div>
+        </div>
+    </article>
+</template>
